922404c0f91676dbf3a9c002028fabda8ee3d9fe
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2021-11-21 13:25:35 -0700

    Add files via upload

---
+++ PieTime/Exercise 5/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Exercise_5
+{
+    partial class PieTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PieTime));
+            this.NumberOfTermsTextBox = new System.Windows.Forms.TextBox();
+            this.NumberOfTermsLabel = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ApproximateValueResult = new System.Windows.Forms.Label();
+            this.CalculateResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NumberOfTermsTextBox
+            // 
+            this.NumberOfTermsTextBox.Location = new System.Drawing.Point(112, 6);
+            this.NumberOfTermsTextBox.Name = "NumberOfTermsTextBox";
+            this.NumberOfTermsTextBox.Size = new System.Drawing.Size(144, 20);
+            this.NumberOfTermsTextBox.TabIndex = 0;
+            this.NumberOfTermsTextBox.TextChanged += new System.EventHandler(this.NumberOfTermsTextBox_TextChanged);
+            // 
+            // NumberOfTermsLabel
+            // 
+            this.NumberOfTermsLabel.AutoSize = true;
+            this.NumberOfTermsLabel.Location = new System.Drawing.Point(12, 9);
+            this.NumberOfTermsLabel.Name = "NumberOfTermsLabel";
+            this.NumberOfTermsLabel.Size = new System.Drawing.Size(89, 13);
+            this.NumberOfTermsLabel.TabIndex = 1;
+            this.NumberOfTermsLabel.Text = "Enter # of Terms:";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(26, 46);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 2;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ApproximateValueResult
+            // 
+            this.ApproximateValueResult.Location = new System.Drawing.Point(23, 92);
+            this.ApproximateValueResult.Name = "ApproximateValueResult";
+            this.ApproximateValueResult.Size = new System.Drawing.Size(233, 23);
+            this.ApproximateValueResult.TabIndex = 3;
+            this.ApproximateValueResult.Click += new System.EventHandler(this.ApproximateValueResult_Click);
+            // 
+            // CalculateResult
+            // 
+            this.CalculateResult.Location = new System.Drawing.Point(23, 136);
+            this.CalculateResult.Name = "CalculateResult";
+            this.CalculateResult.Size = new System.Drawing.Size(233, 27);
+            this.CalculateResult.TabIndex = 4;
+            this.CalculateResult.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // PieTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 173);
+            this.Controls.Add(this.CalculateResult);
+            this.Controls.Add(this.ApproximateValueResult);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.NumberOfTermsLabel);
+            this.Controls.Add(this.NumberOfTermsTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PieTime";
+            this.Text = "PieTime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumberOfTermsTextBox;
+        private System.Windows.Forms.Label NumberOfTermsLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label ApproximateValueResult;
+        private System.Windows.Forms.Label CalculateResult;
+    }
+}
+
---
+++ PieTime/Exercise 5/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_5
+{
+    public partial class PieTime : Form
+    {
+        public PieTime()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //Declaring Variables
+            decimal terms;
+            decimal results = 1;
+            
+
+            //This tests the entry to confirm it is a number
+            if (decimal.TryParse(NumberOfTermsTextBox.Text, out terms))
+
+                // Here we start a loop that iterates over the count of the number entered
+            for(decimal count = 0; count <= terms; count++)
+                {
+                    //This creates a multiplies the iteration by 2 then adds 3 creating 
+                    // a series of incrementing odd numbers
+                    decimal denominator = count * 2 + 3;
+                    
+                    //This tests if the iteration is even or odd
+                    if (count % 2 == 0)
+                    {
+                        //then outputs a minus
+                        results -= (1 / denominator);
+                    }
+                    else
+                    {
+                        //or a positive
+                        results += (1 / denominator);
+                    }
+                }
+
+            else
+            {
+                //this is a window that pops up if the entry is not a number
+                MessageBox.Show("Please enter a valid number");
+            }
+            
+            //this multiplies whatever the result is by 4 to get a precise pi reading
+            results *= 4;
+
+            //Now we conver the results to a string and display the message
+            CalculateResult.Text = (" = " + results.ToString());
+
+            //With a label that concatenates
+            ApproximateValueResult.Text = ("Approximate value of pi after " + NumberOfTermsTextBox.Text + " Terms");
+
+        }
+
+        private void NumberOfTermsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApproximateValueResult_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
